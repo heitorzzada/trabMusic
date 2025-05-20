47dece72c013662f7179e4735789ad73f904b914
heitorzzada <heitorpontessilva3@gmail.com>
2025-05-20 08:02:50 -0300

    Codigo

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def menu():
         print("1 - Adicionar ")
         print("2 - Listar ")
         print("3 - Excluir")
-        print("4 - Atualizar")
+        print("4 - Editar")
         print("0 - Sair")
 
         opcao = input("Escolha: ")
@@ -24,7 +24,7 @@ def menu():
                 print("1 - Adicionar Gênero")
                 print("2 - Adicionar Álbum")
                 print("3 - Adicionar Música")
-                print("0 - Sair")
+                print("0 - Voltar")
                 opcao_1 = input("Escolha: ")
                 if opcao_1 == "1":
                     nome = input("Nome do gênero: ")
@@ -50,6 +50,14 @@ def menu():
                     genero = Genero.get_by_id(genero_id)
         
                     print("\n--- ÁLBUNS ---")
+                    print("A MUSICA JA POSSUI ALBUM ?\nDIGITE 1 PARA SIM\nDIGITE 2 PARA NÃO")
+                    opc = input("DIGITE : ")
+                    if opc == "1":
+                        exit
+                    elif opc == "2":
+                        nome = input("Nome do álbum: ")
+                        ano = int(input("Ano: "))
+                        Album.create(nome=nome, ano=ano)
                     for a in Album.select():
                         print(f"{a.id} - {a.nome} ({a.ano})")
                     album_id = int(input("ID do álbum: "))
@@ -76,7 +84,6 @@ def menu():
             print("1 - Listar Gêneros")
             print("2 - Listar Álbuns")
             print("3 - Listar Músicas")
-            print("0 - Sair")
             opcao_2 = input("Escolha: ")
             if opcao_2 == "1":
                 for g in Genero.select():
@@ -91,18 +98,62 @@ def menu():
         elif opcao == "3":
             for m in Musica.select():
                 print(f"{m.id} - {m.titulo} | {m.artista} | Álbum: {m.album.nome} | Gênero: {m.genero.nome}")
-                ExID = input ("\nInforme o ID da Música que deseja excluir: ")
-                mus = Musica.get_by_id(ExID)
-                mus.delete_instance()
-                print(f"Musica de ID {m.id} excluida com sucesso" )
-    
-        elif opcao == "0":
-            break
-        else:
-            print("Opção inválida!")
+            ExID = input ("\nInforme o ID da Música que deseja excluir: ")
+            mus = Musica.get_by_id(ExID)
+            mus.delete_instance()
+            print(f"Musica de ID {m.id} excluida com sucesso" )
+        
+        elif opcao == "4":
+            while(True):
+                for m in Musica.select():
+                    print(f"{m.id} - {m.titulo} | {m.artista} | Álbum: {m.album.nome} | Gênero: {m.genero.nome}")
+                MusID = input("\nInforme o ID da Musica que deseja editar: ")
+                mus = Musica.get_by_id(MusID)
+            
+                print(f"\nEditando a música: {mus.titulo} | {mus.artista} | Álbun: {mus.album.nome} | Gênero: {m.genero.nome}")
+                print("\nO que deseja editar?")
+                print("1 - Título")
+                print("2 - Artista")
+                print("3 - Gênero")
+                print("4 - Álbum")
+                print("0 - Cancelar")
+                opcao_edicao = input("Escolha: ")
+                if opcao_edicao == "1":
+                    print(f"Titulo Atual: {mus.titulo}")
+                    novo_titulo = input("Novo título: ")
+                    mus.titulo = novo_titulo
 
+                elif opcao_edicao == "2":
+                    print(f"Artista Atual: {mus.artista}")
+                    novo_artista = input("Novo artista: ")
+                    mus.artista = novo_artista
+                elif opcao_edicao == "3":
+                    print("\n--- GÊNEROS ---")
+                    for g in Genero.select():
+                        print(f"{g.id} - {g.nome}")
+                    novo_genero_id = int(input("Novo ID do gênero: "))
+                    novo_genero = Genero.get_by_id(novo_genero_id)
+                    mus.genero = novo_genero
+                elif opcao_edicao == "4":
+                    print("\n--- ÁLBUNS ---")
+                    for a in Album.select():
+                        print(f"{a.id} - {a.nome} ({a.ano})")
+                    novo_album_id = int(input("Novo ID do álbum: "))
+                    novo_album = Album.get_by_id(novo_album_id)
+                    mus.album = novo_album
+                elif opcao_edicao == "0":
+                    print("Edição cancelada!")
+                    break
+                else:
+                    print("Opção inválida!")
+                mus.save()
+                print(f"Música de ID {MusID} editada com sucesso!")
+
+        elif opcao == "0":
+                    break
+        else:
+            print("Opção inválida!")    
 
 if __name__ == "__main__":
     inicializar()
     menu()
-     
